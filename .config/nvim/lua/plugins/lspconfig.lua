1c5aa7bddcfaf435368bd9c973105cadbcb45752
Ben Record <brecord@cricut.com>
2025-06-09 12:04:11 -0600

    Neovim - bash and zsh

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -2,6 +2,9 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      bashls = { filetypes = { "sh", "zsh" } },
+    },
   },
   keys = function()
     local lsp_clients_popup = require("custom.lsp").lsp_clients_popup
